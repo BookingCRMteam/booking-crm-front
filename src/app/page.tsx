cd54fc3660905ef6975abdcf1f660691f9ac287f
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-06-26 23:19:18 +0200

    test: setup CodiumAI PR-Agent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>Hello Booking CRM team &#128512;</h1>
+        <h1>Hello Booking CRM team! &#128512;</h1>
       </main>
       <footer></footer>
     </div>
