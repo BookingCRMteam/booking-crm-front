bf54274e854d5db07239b279f3a9ef3c4b7a1874
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-09-18 11:05:43 +0300

    refactor: remove unnecessary files

--- src/features/health/api/getStatusApi.ts
+++
@@ -1,6 +0,0 @@
-import { axiosInstance } from '@/shared/api/axiosInstance';
-
-export const getStatusApi = async (): Promise<string> => {
-  const res = await axiosInstance.get<string>('/health');
-  return res.data;
-};
