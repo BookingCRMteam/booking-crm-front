4d794898439800c6c7eac2d24bed9690f7c97b46
Taras Kazymir <kazemir.t23.23@gmail.com>
2025-09-30 10:54:47 +0300

    fix: fixed ssg

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -4,6 +4,8 @@ import { CatalogPage } from '@/pages-layer/catalog/ui/CatalogPage';
 
 import { fetchTours } from '@/entities/tour/api/toursApi';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Catalog() {
   const tours = await fetchTours({ limit: 6, offset: 0 });
   return (
