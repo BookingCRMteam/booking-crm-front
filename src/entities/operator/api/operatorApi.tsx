7b3537567b801f7682ce2c8f2556fc303f5309be
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-10-07 01:14:12 +0200

    fix(operator): remove redundant throw keyword for consistency

--- src/entities/operator/api/operatorApi.tsx
+++ src/entities/operator/api/operatorApi.tsx
@@ -46,7 +46,7 @@ export const operatorApi = {
       const res = await axiosInstance.get<OperatorById>(`/operator/${id}`);
       return res.data;
     } catch (e: unknown) {
-      throw handleApiError(e);
+      handleApiError(e);
     }
   },
 };
