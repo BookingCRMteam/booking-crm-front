d8295df4a111f4a1dbd3c5ae1ae2e5ae688e00d7
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-10-09 21:37:45 +0200

    feat(storybook): add stories for operator components

---
+++ src/pages-layer/operator-public/ui/OperatorHeader/data.ts
@@ -0,0 +1,21 @@
+import { OperatorById } from '@/entities/operator/api/types';
+
+export const mockOperator: OperatorById = {
+  id: 1,
+  email: 'email@example.com',
+  createdAt: '2025-09-29T12:02:12.599Z',
+  updatedAt: '2025-09-29T12:02:12.599Z',
+  userId: 10,
+  companyName: 'Приватна особа',
+  firstName: 'Олена',
+  lastName: 'Петренко',
+  phone: '+380971234567',
+  website: 'https://instagram.com',
+  status: 'approved',
+  description:
+    'Я закохалася в Італію ще десять років тому і з того часу перетворила цю любов на справу свого життя. Мої подорожі — це не «тури», а атмосферні історії, де кожна деталь створює особливий настрій.',
+  philosophy:
+    'Я вірю, що подорож — це про емоції та відчуття. Тому створюю маршрути так, щоб вони залишали не тільки спогади про місця, а й тепло в серці — від келиха вина серед тосканських пагорбів до вечері під звуки фаду в Лісабоні.',
+
+  photo: '/images/public-operator.jpg',
+};
