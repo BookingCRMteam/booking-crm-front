2686a0aff4e7874c29161267cc27422fdaa7b1ba
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2026-02-09 21:09:55 +0200

    fix: block booking when data already exists

--- src/features/booking/utils/handleBookingError.ts
+++ src/features/booking/utils/handleBookingError.ts
@@ -1,11 +1,14 @@
 import { ApiError } from '@/shared/api/handleApiError';
 
-export const handleBookingError = (err: ApiError): string => {
+export const handleBookingError = (err: unknown): string => {
   if (!(err instanceof ApiError)) return 'Сталася невідома помилка';
 
   switch (err.statusCode) {
     case 409:
-      return 'Бронювання вже існує та очікує на оплату';
+      if (err.message === 'This pair has already booked this tour.') {
+        return 'Місця з такими даними пари вже заброньовані! Але Ви можете забронювати для когось іншого :)';
+      }
+      return err.message;
     case 400:
     case 404:
       return 'Нажаль, вільні місця закінчилися';
